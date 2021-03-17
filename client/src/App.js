0fbc9d0cd1c7f7145801659bcb86816b73e68ac2
Glover <glover@Glovers-MacBook-Pro.local>
2021-03-17 19:33:38 +0100

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import Login from './pages/login/Login';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Login from "./Login"
+import Dashboard from "./Dashboard"
 
-const App = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  )
+const code = new URLSearchParams(window.location.search).get("code")
+
+function App() {
+  return code ? <Dashboard code={code} /> : <Login />
 }
 
-export default App;
+export default App
